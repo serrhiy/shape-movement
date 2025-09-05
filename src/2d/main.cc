8203cce418bee75558a4f61bbc0b156d776f8fba
Serhii Lytvynenko <serrheylitvinenko@gmail.com>
2025-09-05 03:39:49 +0300

    Make opengl context

--- src/2d/main.cc
+++ src/2d/main.cc
@@ -12,21 +12,50 @@ constexpr unsigned height = 600;
 constexpr unsigned width = 800;
 constexpr const char* title = "Shape movement 2D";
 
+void onWindowSizeChanged(GLFWwindow* window, int width, int height) {
+  logger::logDebug("Changed window size: {}x{}")(width, height);
+  glViewport(0, 0, width, height);
+}
+
+void onKeyPress(GLFWwindow* window, int key, int scancode, int action, int mods) {
+  logger::logDebug("Key action. Key: {}, action: {}")(key, action);
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
 int main(const int argc, const char* argv[]) {
   logger::open(title);
   logger::logDebug("Start. Version {}.{}")(VERSION_MAJOR, VERSION_MINOR);
 
-  if (!glfwInit()) return -1;
+  if (!glfwInit()) {
+    logger::logError("Cannot initiate glfw")();
+    return -1;
+  }
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   utils::defer defer{ utils::sequence(glfwTerminate, logger::logDebug("Exit")) };
 
   GLFWwindow* window = glfwCreateWindow(width, height, title, nullptr, nullptr);
-  if (!window) return -1;
+  if (!window) {
+    logger::logError("Imposible to create window")();
+    return -1;
+  }
+
+  glfwSetFramebufferSizeCallback(window, onWindowSizeChanged);
+  glfwSetKeyCallback(window, onKeyPress);
 
   glfwMakeContextCurrent(window);
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) return -1;
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    logger::logError("Impossible to load OpenGL functions")();
+    return -1;
+  }
 
   while (!glfwWindowShouldClose(window)) {
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     glfwSwapBuffers(window);
     glfwPollEvents();

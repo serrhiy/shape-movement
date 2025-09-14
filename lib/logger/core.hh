91505f0130d0d33a251379cb1410985a92479b99
Serhii Lytvynenko <serrheylitvinenko@gmail.com>
2025-09-14 17:28:19 +0300

    Remove fmt

--- lib/logger/core.hh
+++ lib/logger/core.hh
@@ -1,10 +1,9 @@
 #pragma once
 
-#include <fmt/format.h>
-#include <fmt/ranges.h>
 #include <syslog.h>
 
 #include <filesystem>
+#include <format>
 #include <iostream>
 #include <source_location>
 #include <string>
@@ -18,9 +17,8 @@ void open(std::string_view name) {
 }
 
 void log(int level, const src_loc& location, std::string_view format,
-         auto&&... args) {
-  const std::string message =
-      fmt::format(fmt::runtime(format), std::forward<decltype(args)>(args)...);
+         std::format_args&& args) {
+  const std::string message = std::vformat(format, args);
   const std::filesystem::path full_path = location.file_name();
   syslog(level, "%s:%d %s", full_path.filename().c_str(), location.line(),
          message.c_str());
@@ -29,28 +27,28 @@ void log(int level, const src_loc& location, std::string_view format,
 auto logInfo(std::string_view format,
              const src_loc& location = src_loc::current()) {
   return [format, location](auto&&... args) {
-    log(LOG_INFO, location, format, std::forward<decltype(args)>(args)...);
+    log(LOG_INFO, location, format, std::make_format_args(args...));
   };
 }
 
 auto logWarning(std::string_view format,
                 const src_loc& location = src_loc::current()) {
   return [format, location](auto&&... args) {
-    log(LOG_WARNING, location, format, std::forward<decltype(args)>(args)...);
+    log(LOG_WARNING, location, format, std::make_format_args(args...));
   };
 }
 
 auto logError(std::string_view format,
               const src_loc& location = src_loc::current()) {
   return [format, location](auto&&... args) {
-    log(LOG_ERR, location, format, std::forward<decltype(args)>(args)...);
+    log(LOG_ERR, location, format, std::make_format_args(args...));
   };
 }
 
 auto logDebug(std::string_view format,
               const src_loc& location = src_loc::current()) {
   return [format, location](auto&&... args) {
-    log(LOG_DEBUG, location, format, std::forward<decltype(args)>(args)...);
+    log(LOG_DEBUG, location, format, std::make_format_args(args...));
   };
 }
 }  // namespace logger
